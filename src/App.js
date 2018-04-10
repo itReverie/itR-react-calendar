917396bea2bdf0650e88412f2aad4128818d2e9f
itReverie <brenda@itreverie.com>
2018-04-10 13:36:01 -0400

    Adding a calendar

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AdjectiveCalendar from './AdjectiveCalendar';
+
+import moment from 'moment';
+const now = moment();
+
+class App extends Component {
+  render(){
+
+   return (<div
+    style={{
+      zIndex: 1000,
+      position: 'relative',
+      width: 900,
+      margin: '20px auto',
+    }}
+  >
+      <div style={{ float: 'left', width: 300 }}>
+        <AdjectiveCalendar defaultValue={now} />
+      </div>
+  </div>);
+  }
+}
+
+export default App;
